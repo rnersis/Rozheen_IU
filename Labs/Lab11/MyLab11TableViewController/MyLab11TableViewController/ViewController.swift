277e84dbbecf87f9e12066ca514b090a046d6298
rnersis <54725683+rnersis@users.noreply.github.com>
2023-02-14 17:03:20 -0500

    complete Lab11

---
+++ Labs/Lab11/MyLab11TableViewController/MyLab11TableViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLab11TableViewController
+//
+//  Created by Rozheen Nersisyan Malhami on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
